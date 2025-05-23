059ffe8e4ba63b212bf0281e76d0db9b8c8b4a07
hamzoo00 <m.haris5084@gmail.com>
2025-05-23 10:36:56 +0500

    Create main.java

---
+++ main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello, Java World!");
+
+        int number1 = 7;
+        int number2 = 3;
+        int result = number1 * number2;
+
+        System.out.println("Multiplication of " + number1 + " and " + number2 + " is: " + result);
+    }
+}
